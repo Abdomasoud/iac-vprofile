f2c4681081846a5ec5fbd96f02d5a01bea4b95b7
Abdomasoud <bodymas333@gmail.com>
2025-01-10 17:21:39 +0200

    first commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions-devops-project"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
